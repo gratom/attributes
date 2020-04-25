bd5724f002235c4d17e623dfa5cf2a94a6a2c489
Gratom\Maks <gratomov@gmail.com>
2020-04-25 22:59:56 +0300

    Simple tests for targeting, what is need to check

---
+++ Attibutes/AttributesProject/Assets/Scripts/Test/MyClass.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Atr]
+public class MyClass : MonoBehaviour
+{
+    [SerializeField]
+    public float f1;
+
+    [SerializeField]
+    private GameObject gject;
+}
---
+++ Attibutes/AttributesProject/Assets/Scripts/Tools/AtrLogic.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Reflection;
+using System;
+
+[InitializeOnLoad]
+public class AtrLogic
+{
+    static AtrLogic()
+    {
+        EditorApplication.delayCall += Wow;
+    }
+
+    private static void Wow()
+    {
+        MonoScript[] sceneActive = GetScriptAssetsOfType<MonoBehaviour>();
+        foreach (MonoScript mono in sceneActive)
+        {
+            Atr attribute = Attribute.GetCustomAttribute(mono.GetClass(), typeof(Atr)) as Atr;
+            if (attribute != null)
+            {
+                //Debug.Log(mono.GetClass());
+                Magic(mono);
+            }
+        }
+    }
+
+    private static void Magic(MonoScript mono)
+    {
+        foreach (FieldInfo field in mono.GetClass().GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
+        {
+            SerializeField attribute = Attribute.GetCustomAttribute(field, typeof(SerializeField)) as SerializeField;
+            if (attribute != null)
+            {
+                Debug.Log(field.FieldType.Name + "  " + field.Name);
+            }
+        }
+    }
+
+    private static MonoScript[] GetScriptAssetsOfType<T>()
+    {
+        MonoScript[] scripts = Resources.FindObjectsOfTypeAll<MonoScript>();
+        List<MonoScript> result = new List<MonoScript>();
+
+        foreach (MonoScript m in scripts)
+        {
+            if (m.GetClass() != null && m.GetClass().IsSubclassOf(typeof(T)))
+            {
+                result.Add(m);
+            }
+        }
+        return result.ToArray();
+    }
+}
